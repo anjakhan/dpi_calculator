dab0ce0618c58548bf54c9964d0d65f3befae4c6
trulli90 <trulli90@gmail.com>
2021-02-19 23:42:00 +0100

    html and css for dpi calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const pgformat = {
+    lg: {
+        width: 11.7,
+        height: 16.5
+    },
+    md: {
+        width: 8.3,
+        height: 11.7
+    },
+    sm: {
+        width: 5.8,
+        height: 8.3
+    }
+}
+
+console.log(pgformat)
